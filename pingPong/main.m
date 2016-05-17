0629940ff4afefbd6b2420628823a69eb44452ab
Thomas Davis <tdavis64@gatech.edu>
2016-05-17 19:45:16 -0400

    Initial Commit

---
+++ pingPong/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pingPong
+//
+//  Created by Thomas Davis on 5/17/16.
+//  Copyright © 2016 Thomas Davis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
