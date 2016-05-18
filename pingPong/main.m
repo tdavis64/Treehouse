29a295a8c9a4f55bec7de3d34bbd20240df40cd0
Thomas Davis <tdavis64@gatech.edu>
2016-05-17 21:59:17 -0400

    initial commit

--- pingPong/main.m
+++ pingPong/main.m
@@ -8,10 +8,44 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+int main(int argc, const char * argv[])
+{
+    int playerOneScore = 0;
+    int playerTwoScore = 0;
+    int pick;
+    
+    NSString *msg;
+    
+    enum playerMap
+    {
+        playerOne = 0,
+        playerTwo = 1
+    };
+    
+    while ((playerOneScore < 21 && playerTwoScore < 21) && ((playerOneScore - playerTwoScore) < 2 || (playerTwoScore - playerOneScore) < 2))
+    {
+        pick = arc4random()%2;
+        
+        if (pick == playerOne)
+        {
+            playerOneScore++;
+        }
+        else if (pick == playerTwo)
+        {
+            playerTwoScore++;
+        }
+        
+        if ((playerOneScore >= 21) && (playerOneScore - playerTwoScore) >= 2)
+        {
+            msg = @"Player One Wins!";
+        }
+        else if ((playerTwoScore >= 21) && (playerTwoScore - playerOneScore) >= 2)
+        {
+            msg = @"Player Two Wins!";
+        }
     }
+    
+    NSLog(@"%@", msg);
+    
     return 0;
 }
